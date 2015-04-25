285559a38ec10c397c3927d5c33d6b013a7afd49
Jose Samuel Berigüete Valero <aislen404@gmail.com>
2015-04-25 08:59:11 +0200

    begin

---
+++ bookmarklet.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Aislen404 on 25/4/15.
+ */
+function bookmarklet() {
+    function callback(x) {
+        test.createTest();
+    }
+
+    var s = document.createElement("script");
+    s.src = "https://raw.githubusercontent.com/aislen404/bookmarklet/master/test.js";
+    if (s.addEventListener) {
+        s.addEventListener("load", callback, false)
+    } else if (s.readyState) {
+        s.onreadystatechange = callback(s);
+    }
+    document.body.appendChild(s);
+}
+
--- test.js
+++ test.js
@@ -1,25 +1,27 @@
-function() {
-    "use strict";
-    var test = {
-        testScript: {
-            0:'check-browser-detection.js',
-            1:'check-compatlist.js',
-            2:'check-compressedcontent.js',
-            3:'check-doctype.js',
-            4:'check-libs.js',
-            5:'check-pluginfree.js',
-            6:'check-w3c-validator.js'
+/*
+* test es un objeto (var) y no un constructor (function) por que solo
+* queremos manejar un solo objeto de tipo test
+*
+*/
 
-        }
+
+test = function(){
+    this.testScript = {
+        0:'check-browser-detection.js',
+        1:'check-compatlist.js',
+        2:'check-compressedcontent.js',
+        3:'check-doctype.js',
+        4:'check-libs.js',
+        5:'check-pluginfree.js',
+        6:'check-w3c-validator.js'
     };
-
-    var TestSuite = function() {
-        alert(test.testScript[0]());
-    };
-
-    test.createTest = function() {
-        return new TestSuite();
-    };
-
-    return test;
 };
+
+testSuite = function() {
+    alert(test.testScript[0]());
+};
+
+test.createTest = function() {
+    return new testSuite();
+};
+
