be83fb04c1e4cea0976fb39beaa5af2fb090a9be
Jose Samuel Berigüete Valero <aislen404@gmail.com>
2015-05-04 20:44:42 +0200

    V0R3F0

--- bookmarklet_2.js
+++ bookmarklet_2.js
@@ -8,18 +8,20 @@ function bookmarklet() {
 
     var headID = document.getElementsByTagName('head')[0];
     var cssNode = document.createElement('link');
-
     cssNode.type = 'text/css';
     cssNode.rel = 'stylesheet';
     cssNode.href = 'https://raw.githubusercontent.com/aislen404/bookmarklet/master/modal.css';
     cssNode.media = 'screen';
     headID.appendChild(cssNode);
 
-    var jsNode = document.createElement('script');
-    jsNode.type = 'text/javascript';
-    jsNode.onload=callback();
-    jsNode.src = 'https://raw.githubusercontent.com/aislen404/bookmarklet/master/test_2.js';
-    headID.appendChild(jsNode);
+    var s = document.createElement("script");
+    s.src = "https://raw.githubusercontent.com/aislen404/bookmarklet/master/test_2.js";
 
+    if (s.addEventListener) {
+        s.addEventListener("load", callback, false)
+    } else if (s.readyState) {
+        s.onreadystatechange = callback(s);
+    }
+    document.body.appendChild(s);
 }
 
