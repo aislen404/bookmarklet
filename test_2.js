1b47496e3156d9100fc1fe2b7e8bd0984af73da2
Jose Samuel Berigüete Valero <aislen404@gmail.com>
2015-05-04 17:51:14 +0200

    V0R3F0
    new version in last weekend

--- test_2.js
+++ test_2.js
@@ -163,42 +163,44 @@ var ieUserAgent_Detection = function (){
 
     // Detect whether or not the browser is IE
     var ieRegex = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-    if (ieRegex.exec(ua) == null)
-        this.exception = "The user agent detected does not contai Internet Explorer.";
-
-    // Get the current "emulated" version of IE
-    this.renderVersion = parseFloat(RegExp.$1);
-    this.version = this.renderVersion;
-
-    // Check the browser version with the rest of the agent string to detect compatibility mode
-    if (ua.indexOf("Trident/6.0") > -1) {
-        if (ua.indexOf("MSIE 7.0") > -1) {
-            this.compatibilityMode = true;
-            this.version = 10;                  // IE 10
-        }
-    }
-    else if (ua.indexOf("Trident/5.0") > -1) {
-        if (ua.indexOf("MSIE 7.0") > -1) {
-            this.compatibilityMode = true;
-            this.version = 9;                   // IE 9
-        }
-    }
-    else if (ua.indexOf("Trident/4.0") > -1) {
-        if (ua.indexOf("MSIE 7.0") > -1) {
-            this.compatibilityMode = true;
-            this.version = 8;                   // IE 8
-        }
-    }
-    else if (ua.indexOf("MSIE 7.0") > -1){
-        this.version = 7;                       // IE 7
+    if (ieRegex.exec(ua) == null){
+        val = "The user agent detected does not containt Internet Explorer.";
     }else{
-        this.version = 6;                       // IE 6
-    }
+        // Get the current "emulated" version of IE
+        this.renderVersion = parseFloat(RegExp.$1);
+        this.version = this.renderVersion;
 
-    var val = "IE" + this.version;
-    if (this.compatibilityMode)
-        val += " Compatibility Mode (IE" + this.renderVersion + " emulation)";
+        // Check the browser version with the rest of the agent string to detect compatibility mode
+        if (ua.indexOf("Trident/6.0") > -1) {
+            if (ua.indexOf("MSIE 7.0") > -1) {
+                this.compatibilityMode = true;
+                this.version = 10;                  // IE 10
+            }
+        }
+        else if (ua.indexOf("Trident/5.0") > -1) {
+            if (ua.indexOf("MSIE 7.0") > -1) {
+                this.compatibilityMode = true;
+                this.version = 9;                   // IE 9
+            }
+        }
+        else if (ua.indexOf("Trident/4.0") > -1) {
+            if (ua.indexOf("MSIE 7.0") > -1) {
+                this.compatibilityMode = true;
+                this.version = 8;                   // IE 8
+            }
+        }
+        else if (ua.indexOf("MSIE 7.0") > -1){
+            this.version = 7;                       // IE 7
+        }else{
+            this.version = 6;                       // IE 6
+        }
+
+        var val = "IE" + this.version;
+        if (this.compatibilityMode)
+            val += " Entering in Compatibility Mode (IE" + this.renderVersion + " emulation)";
         return "We have detected the following IE browser: " + val;
+    }
+        return val;
 };
  
 var xUA_compatible_Detection = function () {
@@ -206,10 +208,14 @@ var xUA_compatible_Detection = function () {
     var x = document.getElementsByTagName("META");
     var txt = "";
     var i;
+    if (x.length == 0)
+        txt = 'no META tags in page !!!';
     for (i = 0; i < x.length; i++) {
 
-        if( x[i].httpEquiv.toString() == 'X-UA-Compatible'){
+        if( x[i].httpEquiv.toString() === 'X-UA-Compatible'){
             txt = txt + 'http-equiv: '+ x[i].httpEquiv + ' content: ' + x[i].content;
+        }else{
+            txt = txt + 'No X-UA-Compatible defined !!!';
         }
     }
 
@@ -316,10 +322,10 @@ var doctype_Detection = function () {
 
 /* feedback helper */
 var log = (function () {
-    var log = "";
+    var log = [];
     return {
-        add: function (msg) { log += msg + "\n"; },
-        show: function () { alert(log); log = ""; }
+        add: function (msg) { log.push(msg); },
+        show: function () { return log; }
     }
 })();
 
@@ -343,5 +349,5 @@ function run() {
     log.add(ieUserAgent_Detection());
 
 
-    log.show();
+    return log.show();
 }
