db0c69b1153ce6ca1618be593b3d2a759f84e3c6
Jose Samuel Berigüete Valero <aislen404@gmail.com>
2015-05-06 11:53:55 +0200

    Create bookmarklet_4.js

---
+++ bookmarklet_4.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Aislen404 on 06/05/15.
+ */
+function () {
+    var s = document.createElement('script');
+	s.type = 'text/javascript';
+    s.src = 'http://www.collembolas.com/test_4.js';
+	s.defer = true;	
+	document.body.appendChild(s);   
+}
+
