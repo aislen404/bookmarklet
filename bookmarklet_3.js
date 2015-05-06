3598f3cc7bdd8257f9d728a85781db0448317ccc
Jose Samuel Berigüete Valero <aislen404@gmail.com>
2015-05-06 19:16:06 +0200

    V0R5F0

--- bookmarklet_3.js
+++ bookmarklet_3.js
@@ -2,17 +2,20 @@
  * Created by Aislen404 on 25/4/15.
  */
 function bookmarklet() {
-    function callback() {
-        execute_test_suite();
-    }
+
 
     var s = document.createElement("script");
+    s.type = 'text/javascript';
     s.src = "https://raw.githubusercontent.com/aislen404/bookmarklet/master/test_3.js";
 
     if (s.addEventListener) {
-        s.addEventListener("load", callback, false)
+        s.addEventListener("load", callback(), false)
     } else if (s.readyState) {
-        s.onreadystatechange = callback(s);
+        s.onreadystatechange = callback();
     }
     document.body.appendChild(s);
+
+    function callback() {
+        execute_test_suite();
+    }
 }
