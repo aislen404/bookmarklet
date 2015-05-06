0b9cfaacb169f8f827c91390451faf225d5585a6
Jose Samuel Berigüete Valero <aislen404@gmail.com>
2015-05-06 23:52:37 +0200

    V0R5F0

--- bookmarklet_4.js
+++ bookmarklet_4.js
@@ -1,11 +1,15 @@
 /**
  * Created by Aislen404 on 06/05/15.
  */
-function () {
-    var s = document.createElement('script');
-	s.type = 'text/javascript';
-    	s.src = 'http://www.collembolas.com/test_4.js';
-	s.defer = true;	
-	document.body.appendChild(s);   
-}
+javascript:(function(){
+    if (document.getElementById('openModal') === null){
+        var s = document.createElement('script');
+        s.type = 'application/javascript';
+        s.src = 'https://raw.githubusercontent.com/aislen404/bookmarklet/master/test_4.js';
+        s.defer = true;
+        document.body.appendChild(s);
+    }else{
+        toggle_visibility();
+    }
+})()
 
